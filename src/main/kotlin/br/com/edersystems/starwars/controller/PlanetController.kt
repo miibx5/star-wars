c84c0a0b7358431be9dbb8b0b30ef1b89e3fa8e1
Eder Costa <miibx5@gmail.com>
2020-09-18 09:04:28 -0300

    Implementing getById endpoint

--- src/main/kotlin/br/com/edersystems/starwars/controller/PlanetController.kt
+++ src/main/kotlin/br/com/edersystems/starwars/controller/PlanetController.kt
@@ -10,17 +10,16 @@ Codification.................: UTF-8
 */
 package br.com.edersystems.starwars.controller
 
+import br.com.edersystems.starwars.controller.request.PaginatedRequest
 import br.com.edersystems.starwars.controller.request.PlanetRequest
+import br.com.edersystems.starwars.controller.response.PaginatedResult
 import br.com.edersystems.starwars.model.entities.Planet
 import br.com.edersystems.starwars.model.service.PlanetService
-
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+
 
 @RestController
 @RequestMapping(value = ["/planet"], produces = [MediaType.APPLICATION_JSON_VALUE])
@@ -29,5 +28,15 @@ class PlanetController(private val service: PlanetService) {
     @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE])
     fun create(@RequestBody request: PlanetRequest): ResponseEntity<Planet> {
         return ResponseEntity.status(HttpStatus.CREATED).body(this.service.create(request))
+    }
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable id: Long): ResponseEntity<Planet> {
+        return ResponseEntity.ok().body(service.getById(id))
+    }
+
+    @GetMapping(consumes = [MediaType.APPLICATION_JSON_VALUE])
+    fun getSchedulesByOwner(@RequestBody request: PaginatedRequest?): ResponseEntity<PaginatedResult<Planet>> {
+        return ResponseEntity.ok(service.gePlanet(request))
     }
 }
--- src/main/kotlin/br/com/edersystems/starwars/model/service/PlanetService.kt
+++ src/main/kotlin/br/com/edersystems/starwars/model/service/PlanetService.kt
@@ -9,19 +9,31 @@ Codification.................: UTF-8
 ...........................................................
 */package br.com.edersystems.starwars.model.service
 
+import br.com.edersystems.starwars.controller.request.PaginatedRequest
 import br.com.edersystems.starwars.controller.request.PlanetRequest
+import br.com.edersystems.starwars.controller.response.PaginatedResult
 import br.com.edersystems.starwars.model.entities.Planet
 import br.com.edersystems.starwars.model.repository.PlanetRepository
 import org.springframework.stereotype.Service
 
+
 @Service
 class PlanetService(private val repository: PlanetRepository) {
 
-    fun create(request: PlanetRequest): Planet {
-        var planet = Planet(request)
-        planet = this.repository.save(planet)
-        return planet
+    fun create(request: PlanetRequest) = this.repository.save(Planet(request))
+
+    fun getById(id: Long): Planet = this.repository.findById(id).orElseGet(null)
+
+    fun gePlanet(request: PaginatedRequest?): PaginatedResult<Planet> {
+        val pagePlanets = request?.let { this.repository.findAll(it) }
+        val planets: List<Planet> = (pagePlanets?.content ?: pagePlanets?.content) as List<Planet>
+        return PaginatedResult(planets, pagePlanets!!.totalElements, request.offset, request.pageSize)
     }
 
-    fun getById(id: Long) = this.repository.findById(id)
+    fun update(request: PlanetRequest): Planet {
+        if (request.id == null) {
+            throw IllegalArgumentException()
+        }
+        return this.create(request)
+    }
 }
